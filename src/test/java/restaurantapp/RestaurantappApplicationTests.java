e2785f471650d16b62235c202548aeb622075169
muthuram06 <muthuram.6565@gmail.com>
2026-05-19 14:09:54 +0530

    Initial backend setup

---
+++ src/test/java/restaurantapp/RestaurantappApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
