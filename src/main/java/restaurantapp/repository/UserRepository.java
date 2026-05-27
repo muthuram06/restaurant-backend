0c9579ffe03238cda70d478cb57e7c320c378ba3
muthuram06 <muthuram.6565@gmail.com>
2026-05-27 22:24:31 +0530

    Fixed Dockerfile location

--- src/main/java/restaurantapp/repository/UserRepository.java
+++ src/main/java/restaurantapp/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package restaurantapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import restaurantapp.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
