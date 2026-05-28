6eaf363a2e79f1e0a9490bce321796b886a3d4cd
muthuram06 <muthuram.6565@gmail.com>
2026-05-28 17:31:58 +0530

    fixed order service

--- restaurant-frontend/src/services/OrderService.js
+++ restaurant-frontend/src/services/OrderService.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 
 const API_URL =
-    "http://localhost:8080/api/orders";
+  "https://restaurant-backend-ca51.onrender.com/api/orders";
 
 export const saveOrder = (order) => {
-    return axios.post(API_URL, order);
+  return axios.post(API_URL, order);
 };
 
 export const getAllOrders = () => {
-    return axios.get(API_URL);
+  return axios.get(API_URL);
 };
 
 export const getUserOrders = (email) => {
-    return axios.get(
-        `${API_URL}/user/${email}`
-    );
+  return axios.get(
+    `${API_URL}/user/${email}`
+  );
 };
 
 export const updateOrderStatus = (
-    id,
-    status
+  id,
+  status
 ) => {
 
-    return axios.put(
-        `${API_URL}/${id}/${status}`
-    );
+  return axios.put(
+    `${API_URL}/${id}/${status}`
+  );
 };
