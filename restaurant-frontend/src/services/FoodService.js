664e4d2aec2d82aadd3a1b7c7c979bfc046bf723
muthuram06 <muthuram.6565@gmail.com>
2026-05-28 12:40:10 +0530

    Fixed production API

--- restaurant-frontend/src/services/FoodService.js
+++ restaurant-frontend/src/services/FoodService.js
@@ -2,14 +2,22 @@ import axios from "axios";
 
 const API_URL = "https://restaurant-backend-ca51.onrender.com/api/food";
 
-export const getAllFoods = () =>
-    axios.get(`${API_URL}/all`);
+export const getAllFoods = async () => {
+  const response = await axios.get(`${API_URL}/all`);
+  return response.data;
+};
 
-export const addFood = (food) =>
-    axios.post(`${API_URL}/add`, food);
+export const addFood = async (food) => {
+  const response = await axios.post(`${API_URL}/add`, food);
+  return response.data;
+};
 
-export const deleteFood = (id) =>
-    axios.delete(`${API_URL}/delete/${id}`);
+export const deleteFood = async (id) => {
+  const response = await axios.delete(`${API_URL}/delete/${id}`);
+  return response.data;
+};
 
-export const updateFood = (id, food) =>
-    axios.put(`${API_URL}/update/${id}`, food);
+export const updateFood = async (id, food) => {
+  const response = await axios.put(`${API_URL}/update/${id}`, food);
+  return response.data;
+};
