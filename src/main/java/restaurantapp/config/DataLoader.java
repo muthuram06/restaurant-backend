f02a61e4ab01300b84398d192f9e40fea64a8f9c
muthuram06 <muthuram.6565@gmail.com>
2026-05-29 16:40:54 +0530

    Fix DataLoader

--- src/main/java/restaurantapp/config/DataLoader.java
+++ src/main/java/restaurantapp/config/DataLoader.java
@@ -1,44 +1,53 @@
-foodRepository.save(
-    new FoodItem(
-        "Paneer Butter Masala",
-        "Creamy paneer curry",
-        220,
-        "https://images.unsplash.com/photo-1631452180519-c014fe946bc7"
-    )
-);
+package restaurantapp.config;
 
-foodRepository.save(
-    new FoodItem(
-        "Veg Biryani",
-        "Delicious veg biryani",
-        180,
-        "https://images.unsplash.com/photo-1563379091339-03246963d29a"
-    )
-);
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-foodRepository.save(
-    new FoodItem(
-        "Masala Dosa",
-        "Crispy masala dosa",
-        120,
-        "https://images.unsplash.com/photo-1589302168068-964664d93dc0"
-    )
-);
+import restaurantapp.model.FoodItem;
+import restaurantapp.repository.FoodItemRepository;
 
-foodRepository.save(
-    new FoodItem(
-        "Gobi Manchurian",
-        "Spicy gobi manchurian",
-        160,
-        "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4"
-    )
-);
+@Component
+public class DataLoader implements CommandLineRunner {
 
-foodRepository.save(
-    new FoodItem(
-        "Veg Fried Rice",
-        "Tasty veg fried rice",
-        170,
-        "https://images.unsplash.com/photo-1603133872878-684f208fb84b"
-    )
-);
+    private final FoodItemRepository foodRepository;
+
+    public DataLoader(FoodItemRepository foodRepository) {
+        this.foodRepository = foodRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+
+        if (foodRepository.count() == 0) {
+
+            foodRepository.save(
+                new FoodItem(
+                    "Paneer Butter Masala",
+                    "North Indian",
+                    220,
+                    "https://images.unsplash.com/photo-1603894584373-5ac82b2ae398"
+                )
+            );
+
+            foodRepository.save(
+                new FoodItem(
+                    "Masala Dosa",
+                    "South Indian",
+                    120,
+                    "https://images.unsplash.com/photo-1589302168068-964664493dc0"
+                )
+            );
+
+            foodRepository.save(
+                new FoodItem(
+                    "Veg Biryani",
+                    "Fast Food",
+                    180,
+                    "https://images.unsplash.com/photo-1563379091339-03246963d29a"
+                )
+            );
+
+            System.out.println("Foods inserted successfully!");
+        }
+    }
+}
