53cd503b7071c8bf00cda8e3d7a58762691536e6
muthuram06 <muthuram.6565@gmail.com>
2026-05-30 16:48:48 +0530

    Added cart functionality

--- restaurant-frontend/src/pages/Home.js
+++ restaurant-frontend/src/pages/Home.js
@@ -15,13 +15,12 @@ function Home() {
   }, []);
 
   const fetchFoods = async () => {
+
     try {
 
       const response = await axios.get(
         "https://restaurant-backend-ca51.onrender.com/api/food/all"
       );
-
-      console.log("Foods Loaded:", response.data);
 
       setFoods(response.data);
 
@@ -32,16 +31,60 @@ function Home() {
     }
   };
 
+  const addToCart = (food) => {
+
+    let cart =
+      JSON.parse(
+        localStorage.getItem("cart")
+      ) || [];
+
+    const existingFood =
+      cart.find(
+        (item) => item.id === food.id
+      );
+
+    if (existingFood) {
+
+      existingFood.quantity += 1;
+
+    } else {
+
+      cart.push({
+
+        ...food,
+
+        quantity: 1
+
+      });
+    }
+
+    localStorage.setItem(
+      "cart",
+      JSON.stringify(cart)
+    );
+
+    alert("Food Added To Cart");
+
+    window.location.reload();
+  };
+
   const filteredFoods = foods.filter((food) => {
 
     const matchesSearch =
-      food.name.toLowerCase().includes(search.toLowerCase());
+      food.name
+        .toLowerCase()
+        .includes(
+          search.toLowerCase()
+        );
 
     const matchesCategory =
       category === "All" ||
       food.category === category;
 
-    return matchesSearch && matchesCategory;
+    return (
+      matchesSearch &&
+      matchesCategory
+    );
   });
 
   return (
@@ -71,44 +114,58 @@ function Home() {
           className="form-control mb-4"
           placeholder="🔍 Search Veg Foods..."
           value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={(e) =>
+            setSearch(e.target.value)
+          }
         />
 
         <div className="mb-4">
+
           <button
             className="btn btn-dark me-2"
-            onClick={() => setCategory("All")}
+            onClick={() =>
+              setCategory("All")
+            }
           >
             All
           </button>
 
           <button
             className="btn btn-success me-2"
-            onClick={() => setCategory("Veg")}
+            onClick={() =>
+              setCategory("Veg")
+            }
           >
             Veg
           </button>
 
           <button
             className="btn btn-primary me-2"
-            onClick={() => setCategory("North Indian")}
+            onClick={() =>
+              setCategory("North Indian")
+            }
           >
             North Indian
           </button>
 
           <button
             className="btn btn-danger me-2"
-            onClick={() => setCategory("South Indian")}
+            onClick={() =>
+              setCategory("South Indian")
+            }
           >
             South Indian
           </button>
 
           <button
             className="btn btn-warning"
-            onClick={() => setCategory("Fast Food")}
+            onClick={() =>
+              setCategory("Fast Food")
+            }
           >
             Fast Food
           </button>
+
         </div>
 
         <div className="row">
@@ -145,6 +202,15 @@ function Home() {
                   <span className="badge bg-success">
                     {food.category}
                   </span>
+
+                  <button
+                    className="btn btn-primary w-100 mt-3"
+                    onClick={() =>
+                      addToCart(food)
+                    }
+                  >
+                    Add To Cart
+                  </button>
 
                 </div>
 
